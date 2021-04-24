cc6e62d6b20d89f139d2bc635779c32034979bb9
tmddusgood <tmddusgood@gmail.com>
2021-04-24 11:26:31 +0900

    test: fix: Delete not-using library

--- src/test/java/practice/domain/JaxbMovieReaderTest.java
+++ src/test/java/practice/domain/JaxbMovieReaderTest.java
@@ -1,7 +1,6 @@
 package practice.domain;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
